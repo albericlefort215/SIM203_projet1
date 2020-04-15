3d0bf98a8117e47ae1a1bcc8a86bded292cab66f
albericlefort215 <48726048+albericlefort215@users.noreply.github.com>
2020-04-15 12:34:46 +0200

    Update mesh.c

--- mesh.c
+++ mesh.c
@@ -1,10 +1,4 @@
-//
-//  mesh_albe.c
-
-#include <stdio.h>
 #include <mesh.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
 
 int lnofa[4][4] = {{1,2,3,0},{2,3,0,1},{3,0,1,2},{0,1,2,3}};
@@ -21,20 +15,20 @@ Mesh * msh_init()
   msh->NbrTet = 0;
   
   msh->Ver = NULL;
-  msh->Tri = NULL;
-  msh->Tet = NULL;
+  msh->Tri = NULL;  
+  msh->Tet = NULL; 
  
   
   msh->bb[0] = 0.0; /* xmin  */
-  msh->bb[1] = 0.0; /* ymin  */
-  msh->bb[2] = 0.0; /* zmin  */
-  msh->bb[3] = 0.0; /* xmax  */
-  msh->bb[4] = 0.0; /* ymax  */
-  msh->bb[5] = 0.0; /* zmax  */
+  msh->bb[1] = 0.0; /* xmax  */
+  msh->bb[2] = 0.0; /* ymin  */
+  msh->bb[3] = 0.0; /* ymax  */
+  msh->bb[4] = 0.0; /* zmin  */
+  msh->bb[5] = 0.0; /* zmax  */ 
   
   return msh;
   
-}
+}  
   
   
 Mesh * msh_read(char *file)
@@ -43,7 +37,7 @@ Mesh * msh_read(char *file)
   float  bufFlt[3];
   double bufDbl[3];
   int    i,bufTet[5],bufFac[4];
-  int    FilVer, ref;
+  int    FilVer, ref; 
   
   int64_t fmsh = 0;
   
@@ -51,12 +45,12 @@ Mesh * msh_read(char *file)
   
   Mesh * msh = msh_init();
     
-  //--- set file name
+  //--- set file name 
   strcpy(InpFil,file);
   if ( strstr(InpFil,".mesh") ) {
     if ( !(fmsh = GmfOpenMesh(InpFil,GmfRead,&FilVer,&msh->Dim)) ) {
       return NULL;
-    }
+    }    
   }
   else {
     strcat(InpFil,".meshb");
@@ -65,8 +59,8 @@ Mesh * msh_read(char *file)
       strcat(InpFil,".mesh");
       if ( !(fmsh = GmfOpenMesh(InpFil,GmfRead,&FilVer,&msh->Dim)) ) {
         return NULL;
-      }
-    }
+      }    
+    } 
   }
   
   printf(" File %s opened Dimension %d Version %d \n",InpFil,msh->Dim, FilVer);
@@ -77,13 +71,13 @@ Mesh * msh_read(char *file)
   
   /* allocate arrays */
   msh->Ver = calloc( (msh->NbrVer+1), sizeof(Vertex)       );
-  msh->Tri = calloc( (msh->NbrTri+1), sizeof(Triangle)     );
-  msh->Tet = calloc( (msh->NbrTet+1), sizeof(Tetrahedron)  );
+  msh->Tri = calloc( (msh->NbrTri+1), sizeof(Triangle)     );  
+  msh->Tet = calloc( (msh->NbrTet+1), sizeof(Tetrahedron)  ); 
   
   
    GmfGotoKwd(fmsh, GmfVertices);
    if ( msh->Dim == 2 ) {
-     if ( FilVer == GmfFloat ) {        // read 32 bits float
+     if ( FilVer == GmfFloat ) {		// read 32 bits float
        for (i=1; i<=msh->NbrVer; ++i) {
          GmfGetLin(fmsh, GmfVertices, &bufFlt[0], &bufFlt[1], &ref);
          msh->Ver[i].Crd[0] = (double)bufFlt[0];
@@ -91,17 +85,17 @@ Mesh * msh_read(char *file)
          msh->Ver[i].Crd[2] = 0.0;
        }
      }
-     else  {    // read 64 bits float
+     else  {	// read 64 bits float
        for (i=1; i<=msh->NbrVer; ++i) {
          GmfGetLin(fmsh, GmfVertices, &bufDbl[0], &bufDbl[1], &ref);
          msh->Ver[i].Crd[0] = bufDbl[0];
          msh->Ver[i].Crd[1] = bufDbl[1];
          msh->Ver[i].Crd[2] = 0.0;
-       }
+       }  
      }
    }
    else {
-     if ( FilVer == GmfFloat ) {        // read 32 bits float
+     if ( FilVer == GmfFloat ) {		// read 32 bits float
        for (i=1; i<=msh->NbrVer; ++i) {
          GmfGetLin(fmsh, GmfVertices, &bufFlt[0], &bufFlt[1], &bufFlt[2], &ref);
          msh->Ver[i].Crd[0] = (double)bufFlt[0];
@@ -109,18 +103,18 @@ Mesh * msh_read(char *file)
          msh->Ver[i].Crd[2] = (double)bufFlt[2];
        }
      }
-     else  {    // read 64 bits float
+     else  {	// read 64 bits float
        for (i=1; i<=msh->NbrVer; ++i) {
          GmfGetLin(fmsh, GmfVertices, &bufDbl[0], &bufDbl[1], &bufDbl[2], &ref);
          msh->Ver[i].Crd[0] = bufDbl[0];
          msh->Ver[i].Crd[1] = bufDbl[1];
          msh->Ver[i].Crd[2] = bufDbl[2];
-       }
+       }  
      }
    }
    
   
-   //--- read tetrahedra
+   //--- read tetrahedra 
   GmfGotoKwd(fmsh, GmfTetrahedra);
   for (i=1; i<=msh->NbrTet; ++i) {
     GmfGetLin(fmsh, GmfTetrahedra, &bufTet[0], &bufTet[1], &bufTet[2], &bufTet[3], &bufTet[4]);
@@ -169,6 +163,63 @@ int compar_tetrahedron(const void *a, const void *b)
 }
 
 
+int msh_reorder(Mesh *msh)
+{
+  
+  int iTet, iTri, iVer;
+  
+  if ( ! msh            ) return 0;
+  if ( msh->NbrVer <= 0 ) return 0;
+  
+  /* compute bonding box */
+  msh->bb[0] = msh->Ver[1].Crd[0];
+  msh->bb[1] = msh->Ver[1].Crd[1];
+  msh->bb[2] = msh->Ver[1].Crd[2];
+  msh->bb[3] = msh->Ver[1].Crd[0];
+  msh->bb[4] = msh->Ver[1].Crd[1];
+  msh->bb[5] = msh->Ver[1].Crd[2];
+  for(iVer=1; iVer<=msh->NbrVer; iVer++) {
+    /* todo msh->bb : used to compute the Z-curve index */
+    double x = msh->Ver[iVer].Crd[0];
+    double y = msh->Ver[iVer].Crd[1];
+    double z = msh->Ver[iVer].Crd[2];
+    if(msh->bb[0] > x){msh->bb[0] = x;}
+    if(msh->bb[1] > y){msh->bb[1] = y;}
+    if(msh->bb[2] > z){msh->bb[2] = z;}
+    if(msh->bb[3] < x){msh->bb[3] = x;}
+    if(msh->bb[4] < y){msh->bb[4] = y;}
+    if(msh->bb[5] < z){msh->bb[5] = z;}
+  }
+  
+  for(iVer=1; iVer<=msh->NbrVer; iVer++) {
+    msh->Ver[iVer].icrit  = rand();   /* change the randon  by Z  order */
+    msh->Ver[iVer].idxNew = iVer;
+    msh->Ver[iVer].idxOld = iVer;
+  }
+  //qsort(&msh->Ver[1],msh->NbrVer,sizeof(Vertex), compar_vertex); 
+
+  
+  /* update idxNew for vertices */
+   
+  
+  /* re-assign triangles and tets ids */
+  
+  
+  /* sort triangles */
+  for(iTri=1; iTri<=msh->NbrTri; iTri++) {
+    msh->Tri[iTri].icrit  = rand();  
+  }
+  qsort(&msh->Tri[1],msh->NbrTri,sizeof(Triangle), compar_triangle);
+  
+  /* sort tetrahedra */
+  for(iTet=1; iTet<=msh->NbrTet; iTet++) {
+    msh->Tet[iTet].icrit  = rand();  
+  }
+  qsort(&msh->Tet[1],msh->NbrTet,sizeof(Tetrahedron), compar_tetrahedron);
+  
+  return 1;
+}
+
 int bounding_box(Mesh *msh){
   int iVer;
   msh->bb[0] = msh->Ver[1].Crd[0];
@@ -192,60 +243,43 @@ int bounding_box(Mesh *msh){
   return 1;
 }
 
-int msh_reorder_rand(Mesh *msh)
-{
-  
-  int iTet, iTri, iVer;
-  
-  if ( ! msh            ) return 0;
-  if ( msh->NbrVer <= 0 ) return 0;
-   int new_add[msh->NbrVer + 1];// on stock les nouveaux iD a l'indice correspondant a l'ancien
-  
+int mesh_reorder_random(Mesh* msh){
+  return 1;
+}
+
+int msh_reorder_z(Mesh* msh){
+  int iVer, iTri, iTet;
+  int new_add[msh->NbrVer + 1];
+  bounding_box(msh);
   for(iVer=1; iVer<=msh->NbrVer; iVer++) {
-    msh->Ver[iVer].icrit  = rand();
+    msh->Ver[iVer].icrit  = get_crit(msh->Ver[iVer], msh->bb, 21);
     msh->Ver[iVer].idxNew = iVer;
     msh->Ver[iVer].idxOld = iVer;
   }
   qsort(&msh->Ver[1],msh->NbrVer,sizeof(Vertex), compar_vertex);
-   
-    /* update idxNew for vertices */
-    for(iVer = 1; iVer <= msh->NbrVer; iVer++){  // on met a jour les id apres le tri
-         
-       msh->Ver[iVer].idxNew = iVer;
-       new_add[msh->Ver[iVer].idxOld] = iVer;
-     }
+  for(iVer = 1; iVer <= msh->NbrVer; iVer++){
+    msh->Ver[iVer].idxNew = iVer;
+    new_add[msh->Ver[iVer].idxOld] = iVer;
+  }
 
-    /* re-assign triangles and tets ids */
-    int j;
-      for(iTri = 1; iTri <= msh->NbrTri; iTri++){
-       msh->Tri[iTri].icrit = msh->NbrVer;
-       for(int j = 0; j < 3; j++){
-         msh->Tri[iTri].Ver[j] = new_add[msh->Tri[iTri].Ver[j]]; //mise a jour des numeros des sommets formant les triangles
-    
-       }
-     }
-   
-
-   for(iTet = 1; iTet <= msh->NbrTet; iTet++){
-     msh->Tet[iTet].icrit = msh->NbrVer;
-     for(int j = 0; j < 4; j++){
-       msh->Tet[iTet].Ver[j] = new_add[msh->Tet[iTet].Ver[j]];
-      
-     }
-   }
-
-  /* sort triangles */
-  for(iTri=1; iTri<=msh->NbrTri; iTri++) {
-    msh->Tri[iTri].icrit  = rand();
+  for(iTri = 1; iTri <= msh->NbrTri; iTri++){
+    msh->Tri[iTri].icrit = msh->NbrVer;
+    for(int j = 0; j < 3; j++){
+      msh->Tri[iTri].Ver[j] = new_add[msh->Tri[iTri].Ver[j]];
+      if(msh->Tri[iTri].icrit > msh->Tri[iTri].Ver[j]){msh->Tri[iTri].icrit = msh->Tri[iTri].Ver[j];}
+    }
   }
   qsort(&msh->Tri[1],msh->NbrTri,sizeof(Triangle), compar_triangle);
-  
-  /* sort tetrahedra */
-  for(iTet=1; iTet<=msh->NbrTet; iTet++) {
-    msh->Tet[iTet].icrit  = rand();
+
+  for(iTet = 1; iTet <= msh->NbrTet; iTet++){
+    msh->Tet[iTet].icrit = msh->NbrVer;
+    for(int j = 0; j < 4; j++){
+      msh->Tet[iTet].Ver[j] = new_add[msh->Tet[iTet].Ver[j]];
+      if(msh->Tet[iTet].icrit > msh->Tet[iTet].Ver[j]){msh->Tet[iTet].icrit = msh->Tet[iTet].Ver[j];}
+    }
   }
   qsort(&msh->Tet[1],msh->NbrTet,sizeof(Tetrahedron), compar_tetrahedron);
-  
+
   return 1;
 }
 
@@ -262,54 +296,15 @@ long long int get_crit(Vertex v, double* bb, int depth){
     int bz = (int) z > zmid;
     int by = (int) y > ymid;
     int bx = (int) x > xmid;
-    crit = bz << 2 + (bz ^ by) << 1 + (bz ^ bx);
+    crit = (bz << 2) + ((bz ^ by) << 1) + (bz ^ bx);
     double new_bb[6] = {bb[0] + bx * xmid, bb[1] + by * ymid, bb[2] + bz * zmid, bb[3] - (1 - bx) * xmid, bb[4] - (1 - by) * ymid, bb[5] - (1 - bz) * zmid};
     return crit << (3 * (depth - 1)) + get_crit(v, new_bb, depth - 1);
   }
 }
 
-int msh_reorder_z(Mesh* msh){
-  int iVer, iTri, iTet;
-  int new_add[msh->NbrVer + 1];// on stock les nouveaux iD a l'indice correspondant a l'ancien
-  bounding_box(msh);
-  for(iVer=1; iVer<=msh->NbrVer; iVer++) {
-    msh->Ver[iVer].icrit  = get_crit(msh->Ver[iVer], msh->bb, 21);// critere de la courbe en z
-    msh->Ver[iVer].idxNew = iVer;
-    msh->Ver[iVer].idxOld = iVer;
-  }
-  qsort(&msh->Ver[1],msh->NbrVer,sizeof(Vertex), compar_vertex);
-  for(iVer = 1; iVer <= msh->NbrVer; iVer++){  // on met a jour les id apres le tri
-      
-    msh->Ver[iVer].idxNew = iVer;
-    new_add[msh->Ver[iVer].idxOld] = iVer;
-  }
- 
-  for(iTri = 1; iTri <= msh->NbrTri; iTri++){
-    int j;
-    msh->Tri[iTri].icrit = msh->NbrVer;
-    for(int j = 0; j < 3; j++){
-      msh->Tri[iTri].Ver[j] = new_add[msh->Tri[iTri].Ver[j]]; //mise a jour des numeros des sommets formant les triangles
-      if(msh->Tri[iTri].icrit > msh->Tri[iTri].Ver[j]){msh->Tri[iTri].icrit = msh->Tri[iTri].Ver[j];} // mise a jour du critere du triangle (= plus petit index dans le triangle)
-    }
-  }
-  qsort(&msh->Tri[1],msh->NbrTri,sizeof(Triangle), compar_triangle); 
-
-  for(iTet = 1; iTet <= msh->NbrTet; iTet++){
-    msh->Tet[iTet].icrit = msh->NbrVer;
-    for(int j = 0; j < 4; j++){
-      msh->Tet[iTet].Ver[j] = new_add[msh->Tet[iTet].Ver[j]];
-      if(msh->Tet[iTet].icrit > msh->Tet[iTet].Ver[j]){msh->Tet[iTet].icrit = msh->Tet[iTet].Ver[j];}
-    }
-  }
-  qsort(&msh->Tet[1],msh->NbrTet,sizeof(Tetrahedron), compar_tetrahedron);
-
-  return 1;
-}
-
-
 int    msh_write(Mesh *msh, char *file)
 {
-   int iVer, iTfr, iTet;
+   int iVer, iTri, iTfr, iTet;
    int FilVer = 2;
   
    if ( ! msh  ) return 0;
@@ -324,21 +319,21 @@ int    msh_write(Mesh *msh, char *file)
    GmfSetKwd(fmsh, GmfVertices,   msh->NbrVer);
    if ( msh->Dim == 3 ) {
      for ( iVer=1; iVer<=msh->NbrVer; iVer++){
-       GmfSetLin(fmsh, GmfVertices, msh->Ver[iVer].Crd[0],msh->Ver[iVer].Crd[1],msh->Ver[iVer].Crd[2],0);
+       GmfSetLin(fmsh, GmfVertices, msh->Ver[iVer].Crd[0],msh->Ver[iVer].Crd[1],msh->Ver[iVer].Crd[2],0); 
      }
    }
    else {
      for ( iVer=1; iVer<=msh->NbrVer; iVer++){
-       GmfSetLin(fmsh, GmfVertices, msh->Ver[iVer].Crd[0],msh->Ver[iVer].Crd[1],0);
+       GmfSetLin(fmsh, GmfVertices, msh->Ver[iVer].Crd[0],msh->Ver[iVer].Crd[1],0); 
      }
    }
    
    GmfSetKwd(fmsh, GmfTriangles, msh->NbrTri);
-   for ( iTfr=1; iTfr<=msh->NbrTri; iTfr++)
-     GmfSetLin(fmsh, GmfTriangles, msh->Tri[iTfr].Ver[0], msh->Tri[iTfr].Ver[1], msh->Tri[iTfr].Ver[2], msh->Tri[iTfr].Ref);
+   for ( iTfr=1; iTfr<=msh->NbrTri; iTfr++)  
+     GmfSetLin(fmsh, GmfTriangles, msh->Tri[iTfr].Ver[0], msh->Tri[iTfr].Ver[1], msh->Tri[iTfr].Ver[2], msh->Tri[iTfr].Ref);  
    
    GmfSetKwd(fmsh, GmfTetrahedra, msh->NbrTet);
-   for ( iTet=1; iTet<=msh->NbrTet; iTet++)
+   for ( iTet=1; iTet<=msh->NbrTet; iTet++) 
      GmfSetLin(fmsh, GmfTetrahedra, msh->Tet[iTet].Ver[0], msh->Tet[iTet].Ver[1],msh->Tet[iTet].Ver[2], msh->Tet[iTet].Ver[3], 0);
    
    GmfCloseMesh(fmsh);
@@ -347,53 +342,69 @@ int    msh_write(Mesh *msh, char *file)
    
 }
 
-bool est_egal(int i1,int i2,int i3,int j1,int j2,int j3)
-{
-int a,b,c;
-a=(i1-j1)*(i1-j2)*(i1-j3);
-b=(i2-j1)*(i2-j2)*(i2-j3);
-c=(i3-j1)*(i3-j2)*(i3-j3);
-return (a==0 && b==0 && c==0);
-}
+
+
 
 
 
 int  msh_neighborsQ2(Mesh *msh)
 {
-  int iTet, iFac, jTet, jFac, ip1, ip2 ,ip3 , jp1, jp2, jp3, it, i_oppose;
+  int iTet, iFac, jTet, jFac, ip1, ip2 ,ip3 , jp1, jp2, jp3;
+  int count = 0;
+  double time_cur = 0, time_est = 0;
+  double to =  GetWallClock();
+  double ti;
   
   if ( ! msh ) return 0;
-		 
+  
   for(iTet=1; iTet<=msh->NbrTet; iTet++) {
-    printf("%i / %i  \n",iTet,msh->NbrTet);
-    for(it=0; it<4; it++) {
-	msh->Tet[iTet].Voi[it]=-1; //initialisation des voisins
-}
     for(iFac=0; iFac<4; iFac++) {
       ip1 = msh->Tet[iTet].Ver[lnofa[iFac][0]];
       ip2 = msh->Tet[iTet].Ver[lnofa[iFac][1]];
       ip3 = msh->Tet[iTet].Ver[lnofa[iFac][2]];
-       // find the Tet different from iTet that has ip1, ip2, ip2 as vertices 
+      /* find the Tet different from iTet that has ip1, ip2, ip2 as vertices */
       for(jTet=1; jTet<=msh->NbrTet; jTet++) {
         if ( iTet == jTet ) continue;
         for(jFac=0; jFac<4; jFac++) {
           jp1 = msh->Tet[jTet].Ver[lnofa[jFac][0]];
           jp2 = msh->Tet[jTet].Ver[lnofa[jFac][1]];
           jp3 = msh->Tet[jTet].Ver[lnofa[jFac][2]];
-          if (est_egal(ip1,ip2,ip3,jp1,jp2,jp3)){
-	    i_oppose=msh->Tet[iTet].Ver[lnofa[iFac][3]]; //j'ai rajouté une dimension à lnofa pour avoir facilement l'indice opposé
-	    msh->Tet[iTet].Voi[lnofa[iFac][3]]=jTet;
-	    }
+          /* compare the 6 points */
+          int a = (ip1 - jp1) & (ip1 - jp2) & (ip1 - jp3);
+          int b = (ip2 - jp1) & (ip2 - jp2) & (ip2 - jp3);
+          int c = (ip3 - jp1) & (ip3 - jp2) & (ip3 - jp3);
+          if(a == 0 && b == 0 && c == 0){
+            msh->Tet[iTet].Voi[lnofa[iFac][3]] = jTet;
+          }
         }
       }
-      
+    }
+    if(iTet%10 == 0){
+      ti =  GetWallClock();
+      time_cur = ti - to;
+      count++;
+      time_est = (time_cur / (10 * count)) * msh->NbrTet;
+      printf("Temps écoulé estimé à ");
+      print_time(time_cur);
+      printf(", temps restant estimé à ");
+      print_time(time_est - time_cur);
+      printf(", temps total estimé à ");
+      print_time(time_est);
+      printf("\n");
     }
   }
   
   return 1;
+}   
+
+void print_time(double t){
+  int h, m, s;
+  s = ((int) t) % 60;
+  m = (((int) t) % 3600) / 60;
+  h = ((int) t) / 3600;
+
+  printf("%d h %d m %d s", h, m, s);
 }
-
-
 
 int  msh_neighbors(Mesh *msh)
 {
@@ -418,3 +429,62 @@ int  msh_neighbors(Mesh *msh)
 }
 
 
+
+HashTable * hash_init(int SizHead, int NbrMaxObj){
+  HashTable * new_table = malloc(sizeof(HashTable));
+  new_table->SizHead = SizHead;
+  new_table->NbrObj = 0;
+  new_table->NbrMaxObj = NbrMaxObj;
+  new_table->Head = malloc(SizHead * sizeof(int));
+  new_table->LstObj = malloc(NbrMaxObj * sizeof(int6));
+  return new_table;
+}
+
+int hash_add(HashTable *hsh, int ip1, int ip2, int ip3, int iTet){
+
+  int key = (ip1 + ip2 + ip3) % hsh->SizHead;
+
+  hsh->NbrObj++;
+  memcpy(&(hsh->LstObj[hsh->NbrObj]), (int6) {ip1, ip2, ip3, iTet, 0, 0}, sizeof(int6));
+
+  if(hsh->Head[key] == 0){
+    hsh->Head[key] = hsh->NbrObj;
+  }else{
+    int6 * element = &(hsh->LstObj[hsh->Head[key]]);
+    while(*element[5] != 0){
+      element = &(hsh->LstObj[*element[5]]);
+    }
+    *element[5] = hsh->NbrObj;
+  }
+  return hsh->NbrObj;
+}
+
+int hash_find(HashTable * hsh, int ip1, int ip2, int ip3){
+  
+  int key = (ip1 + ip2 + ip3) % hsh->SizHead;
+
+  if(hsh->Head[key] == 0){
+    return 0;
+  }else{
+    int next = hsh->Head[key];
+    int search = 1;
+
+    while(search){
+      int jp1 = hsh->LstObj[next][0];
+      int jp2 = hsh->LstObj[next][1];
+      int jp3 = hsh->LstObj[next][2];
+      int a = (ip1 - jp1) & (ip1 - jp2) & (ip1 - jp3);
+      int b = (ip2 - jp1) & (ip2 - jp2) & (ip2 - jp3);
+      int c = (ip3 - jp1) & (ip3 - jp2) & (ip3 - jp3);
+      if(a == 0 && b == 0 && c == 0){
+        search = 0;
+        return next;
+      }
+      next = hsh->LstObj[next][5];
+      if(next == 0){
+        search = 0;
+        return 0;
+      }
+    }
+  }
+}
